32a4532602a613c593a6e5eb5ac5a485f317b546
Sean Crowley <sean.crowley@datirium.com>
2024-11-04 13:03:05 -0500

    SDAPFM-77 include slurm run aspects in run_toil for current public sat

--- run_toil_REAL.sh
+++ run_toil_REAL.sh
@@ -24,6 +24,8 @@ SCRIPT_DIR=${16:-"/home/scidap_satellite/satellite/satellite/bin"}
 JOBSTORE="${TMPDIR}/${DAG_ID}_${RUN_ID}/jobstore"
 LOGS="${TMPDIR}/${DAG_ID}_${RUN_ID}/logs"
 
+WORKFLOW_TABLE="$SCRIPT_DIR/workflow_req_table.csv"
+
     
 # # start progress script in background and kill with this
 bash $SCRIPT_DIR/toil_progress.sh $TMPDIR $DAG_ID $RUN_ID $TOTAL_STEPS $NJS_CLIENT_PORT &
@@ -123,39 +125,200 @@ runClusterMode(){
   curl -X POST http://localhost:${NJS_CLIENT_PORT}/airflow/progress -H "Content-Type: application/json" -d "${PAYLOAD}"
 
 
-  toil-cwl-runner \
-    --logDebug \
-    --stats \
-    --bypass-file-store \
-    --batchSystem slurm \
-    --retryCount 0 \
-    --disableCaching \
-    --defaultMemory ${MEMORY} \
-    --defaultCores ${CPU} \
-    --jobStore "${JOBSTORE}" \
-    --writeLogs ${LOGS} \
-    --outdir ${OUTDIR} ${WORKFLOW} ${JOB} > ${OUTDIR}/results_full.json
-    toil stats ${JOBSTORE} > ${OUTDIR}/stats.txt
-    cat ${OUTDIR}/results_full.json | ${SCRIPT_DIR}/jq 'walk(if type == "object" then with_entries(select(.key | test("listing") | not)) else . end)' > ${OUTDIR}/results.json
-    
-    RESULTS=`cat ${OUTDIR}/results.json`
-    PAYLOAD="{\"payload\":{\"dag_id\": \"${DAG_ID}\", \"run_id\": \"${RUN_ID}\", \"results\": $RESULTS}}"
-    echo $PAYLOAD > "${OUTDIR}/payload.json"
-    echo "Sending workflow execution results from ${OUTDIR}/payload.json"
-    curl -X POST http://localhost:${NJS_CLIENT_PORT}/airflow/results -H "Content-Type: application/json" -d @"${OUTDIR}/payload.json"
+  ## TODO: cpu/mem
 
-    echo "Cleaning temporary directory ${TMPDIR}/${DAG_ID}_${RUN_ID}"
-    rm -rf "${TMPDIR}"
-    pkill -P $progressPID
+  # get names of each workflow step
+  stepNames=$(jq '.steps | to_entries | .[].key' $WORKFLOW)
+  # strip " from stepnames
+  stepNames=$(echo $stepNames | sed -e 's/"//g') 
+  # get steps with resourceRequirements
+  stepsWithReqs=$(jq -r '.steps | to_entries | .[] | select( any(.value.run.requirements.[]; .class == "ResourceRequirement" )) | .key' $WORKFLOW)
+  # echo "stepNames: $stepNames"
+  # echo "stepsWithReqs: $stepsWithReqs"
+
+  # get number steps that dont have resourceReqs
+  stepsWithoutReqs=0
+  for f in $stepNames; 
+  do 
+      if [[ ${stepsWithReqs} != *"$f"* ]];then
+          stepsWithoutReqs=$((stepsWithoutReqs+1))
+      fi
+  done
+  # if stepsWithoutReqs == 0
+    # run without cpu/mem params
+  # else
+    # get label and set params based on low/med/high/NA reqs
+  if [ $stepsWithoutReqs \> 0 ];
+  then
+    echo "some steps dont have resReqs"
+
+    # WORKFLOW_TABLE="/Users/scrowley/Downloads/global_cwls_all_20241008.csv"
+
+    LABEL=$(jq -r '.label' $WORKFLOW)
+    echo "label: ${LABEL}"
+    # LABEL_LINE=$(sed -n '/$LABEL/p' $WORKFLOW)
+    # LABEL_LINE=$(grep -n "${LABEL}" $WORKFLOW)
+    LABEL_LINE=$(grep -n "$LABEL" $WORKFLOW_TABLE)
+    # awk -v line='0 8 * * * Me echo "start working please"' '$0 == line {print "this is the line number", NR, "from", FILENAME}' a
+    echo "label line: $LABEL_LINE"
+
+    ## comma separate and get last entry
+    commaSepLine=()
+    tmpIFS=$IFS
+    IFS=$IFS,
+    for f in $LABEL_LINE; do commaSepLine+=($f); done
+    IFS=$tmpIFS
+    # get last entry
+    JOB_PRIO=${commaSepLine[${#commaSepLine[@]} - 1]} #${commaSepLine[-1]}
+    # strip other chars
+    JOB_PRIO=$(echo $JOB_PRIO | sed -e 's/\r//g')
+
+    case "$JOB_PRIO" in
+      "high")
+        MEMORY=122070 # 128Gb  #61035 64Gb
+        CPU=12
+        ;;
+      "med")
+        MEMORY=30517 # 32Gb
+        CPU=4
+        ;;
+      "low")
+        MEMORY=3814 # 2Gb
+        CPU=1
+        ;;
+      *)
+        MEMORY=30517  # 32Gb
+        CPU=4
+        ;;
+    esac
+
+    echo "MEM: $MEMORY"
+    echo "CPU: $CPU"
+    echo "running toil with assigned cpu/mem"
+    toil-cwl-runner \
+      --logDebug \
+      --stats \
+      --bypass-file-store \
+      --batchSystem slurm \
+      --retryCount 0 \
+      --disableCaching \
+      --defaultMemory ${MEMORY} \
+      --defaultCores ${CPU} \
+      --jobStore "${JOBSTORE}" \
+      --writeLogs ${LOGS} \
+      --outdir ${OUTDIR} ${WORKFLOW} ${JOB} > ${OUTDIR}/results_full.json
+
+  else
+      echo "let toil parse"
+      toil-cwl-runner \
+        --logDebug \
+        --stats \
+        --bypass-file-store \
+        --batchSystem slurm \
+        --retryCount 0 \
+        --disableCaching \
+        --jobStore "${JOBSTORE}" \
+        --writeLogs ${LOGS} \
+        --outdir ${OUTDIR} ${WORKFLOW} ${JOB} > ${OUTDIR}/results_full.json
+  fi
+
+  
+  toil stats ${JOBSTORE} > ${OUTDIR}/stats.txt
+  cat ${OUTDIR}/results_full.json | ${SCRIPT_DIR}/jq 'walk(if type == "object" then with_entries(select(.key | test("listing") | not)) else . end)' > ${OUTDIR}/results.json
+  
+  RESULTS=`cat ${OUTDIR}/results.json`
+  PAYLOAD="{\"payload\":{\"dag_id\": \"${DAG_ID}\", \"run_id\": \"${RUN_ID}\", \"results\": $RESULTS}}"
+  echo $PAYLOAD > "${OUTDIR}/payload.json"
+  echo "Sending workflow execution results from ${OUTDIR}/payload.json"
+  curl -X POST http://localhost:${NJS_CLIENT_PORT}/airflow/results -H "Content-Type: application/json" -d @"${OUTDIR}/payload.json"
+
+  echo "Cleaning temporary directory ${TMPDIR}/${DAG_ID}_${RUN_ID}"
+  rm -rf "${TMPDIR}"
+  pkill -P $progressPID
 
 }
+runLsfClusterMode()
+{
+trap cleanup SIGINT SIGTERM SIGKILL ERR
+replacementStr="\"location\": \"file://$SYSTEM_ROOT"
+# echo $replacementStr
+sed -i "s|\"location\": \"file:///scidap/|$replacementStr|g" $JOB
+
+
+bsub -J "${DAG_ID}_${RUN_ID}" \
+     -M 64000 \
+     -W 48:00 \
+     -n 4 \
+     -R "rusage[mem=64000] span[hosts=1]" \
+     -o "${OUTDIR}/stdout.txt" \
+     -e "${OUTDIR}/stderr.txt" << EOL
+module purge
+module load nodejs jq anaconda3 singularity/3.7.0
+source $TOIL_ENV_FILE
+mkdir -p ${OUTDIR} ${LOGS}
+rm -rf ${JOBSTORE}
+export TMPDIR="${TMPDIR}/${DAG_ID}_${RUN_ID}"
+export SINGULARITY_TMPDIR=$SINGULARITY_TMP_DIR
+export CWL_SINGULARITY_CACHE=$CWL_SINGULARITY_CACHE
+export TOIL_LSF_ARGS="-W 48:00"
+toil-cwl-runner \
+--logDebug \
+--bypass-file-store \
+--batchSystem lsf \
+--singularity \
+--retryCount 0 \
+--clean always \
+--disableCaching \
+--defaultMemory ${MEMORY} \
+--defaultCores ${CPU} \
+--jobStore "${JOBSTORE}" \
+--writeLogs ${LOGS} \
+--outdir ${OUTDIR} ${WORKFLOW} ${JOB} | jq 'walk(if type == "object" then with_entries(select(.key | test("listing") | not)) else . end)' > ${OUTDIR}/results.json
+EOL
+
+# jq 'walk(if type == "object" then with_entries(select(.key | test("listing") | not)) else . end)'
+
+
+bwait -w "started(${DAG_ID}_${RUN_ID})"
+echo "Sending workflow execution progress"
+PAYLOAD="{\"payload\":{\"dag_id\": \"${DAG_ID}\", \"run_id\": \"${RUN_ID}\", \"state\": \"Sent to Cluster\", \"progress\": 8, \"error\": \"\", \"statistics\": \"\", \"logs\": \"\"}}"
+echo $PAYLOAD
+curl -X POST http://localhost:${NJS_CLIENT_PORT}/airflow/progress -H "Content-Type: application/json" -d "${PAYLOAD}"
+
+bwait -w "done(${DAG_ID}_${RUN_ID})"      # won't be caught by trap if job finished successfully
+
+RESULTS=`cat ${OUTDIR}/results.json`
+PAYLOAD="{\"payload\":{\"dag_id\": \"${DAG_ID}\", \"run_id\": \"${RUN_ID}\", \"results\": $RESULTS}}"
+echo $PAYLOAD > "${OUTDIR}/payload.json"
+echo "Sending workflow execution results from ${OUTDIR}/payload.json"
+# kill $progressPID
+curl -X POST http://localhost:${NJS_CLIENT_PORT}/airflow/results -H "Content-Type: application/json" -d @"${OUTDIR}/payload.json"
+
+echo "Cleaning temporary directory ${TMPDIR}/${DAG_ID}_${RUN_ID}"
+bsub -J "${DAG_ID}_${RUN_ID}_cleanup" \
+     -M 16000 \
+     -W 8:00 \
+     -n 2 \
+     -R "rusage[mem=16000] span[hosts=1]" \
+     -o "${OUTDIR}/cleanup_stdout.txt" \
+     -e "${OUTDIR}/cleanup_stderr.txt" << EOL
+rm -rf "${TMPDIR}/${DAG_ID}_${RUN_ID}"
+EOL
+bwait -w "ended(${DAG_ID}_${RUN_ID}_cleanup)"
+# waiting for singularity/toil to formally end before kill progress-process might result in progress sending back a percentage when it shouldnt have
+pkill -P $progressPID
+}
+
 if [ "$BATCH_SYSTEM" = "slurm" ]
 then
-    runClusterMode
+  runClusterMode
 elif [ "$BATCH_SYSTEM" = "single_machine" ]
 then
-    runSingleMode
+  runSingleMode
+elif [ "$BATCH_SYSTEM" = "lsf" ]
+then
+  runLsfClusterMode
 else 
-    echo "BATCH SYSTEM not recognized. job not run"
-    cleanup
+  echo "BATCH SYSTEM not recognized. job not run"
+  cleanup
 fi
